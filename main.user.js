5aa8159d775f96720abf48c9531a7d5b79db64b6
铂屑 <80164656+boxie123@users.noreply.github.com>
2023-11-18 17:19:52 +0800

    引入jquery

--- main.user.js
+++ main.user.js
@@ -9,27 +9,12 @@
 // @include     /https?:\/\/live\.bilibili\.com\/(blanc\/)?\d+\??.*/
 // @icon        https://www.google.com/s2/favicons?domain=bilibili.com
 // @description 鼠标悬停在粉丝勋章上时提供主播信息
+// @require     https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js
 // ==/UserScript==
 
 (async function () {
     'use strict';
 
-    let targetNode = document.getElementById('chat-items');
-    addMouseoverEventForNode(targetNode)
-
-    let observerOptions = {
-        childList: true,
-        subtree: true
-    };
-
-    let observer = new MutationObserver(addMouseoverEvent);
-
-    while (targetNode == null){
-        console.warn(`找不到聊天区元素，等待3秒。`)
-        await new Promise((res,) => setTimeout(res, 3000)) // wait 3 seconds
-    }
-
-    observer.observe(targetNode, observerOptions);
     let anchorInfoCache = {};
 
     async function getAnchorInfo(mid) {
@@ -41,52 +26,38 @@
         return anchorInfoCache[mid];
     }
 
-    function addMouseoverEvent(mutationList, observer) {
-        for (let mutation of mutationList) {
-            if (mutation.type === 'childList') {
-                for (let node of mutation.addedNodes) {
-                    if (!(node instanceof HTMLElement)) continue;
-                    addMouseoverEventForNode(node)
-                }
-            }
-        }
+    while ($('#chat-items').children().length == 0) {
+        console.warn(`找不到弹幕框元素，等待3秒。`)
+        await new Promise((res,) => setTimeout(res, 3000)) // wait 3 seconds
     }
 
-    async function addMouseoverEventForNode(node) {
-        let medals = node.getElementsByClassName('fans-medal-item-ctnr');
-        for (let i = 0; i < medals.length; i++) {
-            if (medals[i].hasAttribute('isObserved')) continue;
-            medals[i].setAttribute('isObserved', '');
-            medals[i].addEventListener('mouseenter', async function (e) {
-                let roomId = this.getAttribute('data-room-id');
-                let mid = this.getAttribute('data-anchor-id');
-                let anchorInfo = await getAnchorInfo(mid);
-
-                let image = document.createElement('img');
-                image.src = anchorInfo.face;
-                image.style = "border-radius: 50%; width: 64px; height: 64px";
-
-                let anchor = document.createElement("a");
-                anchor.href = `https://live.bilibili.com/${roomId}`;
-                anchor.target = "_blank";
-                anchor.style = "position: absolute; top: 0; left: 0; background-color: #fff; border: 1px solid #000; padding: 5px; z-index: 999; display: flex; align-items: center; text-decoration: none; color: black";
-
-                let text = document.createElement('span');
-                text.innerText = anchorInfo.name;
-
-                anchor.appendChild(image);
-                anchor.appendChild(text);
-
-                this.appendChild(anchor);
-                this.lastChild.style.display = "block";
-            });
-
-            medals[i].addEventListener('mouseleave', function (e) {
-                this.lastChild.style.display = "none";
-            });
+    $(document).on('mouseenter', '.fans-medal-item-ctnr', async function (e) {
+        if ($(this).hasClass('isObserved')) {
+            $(this).find('.medal-anchor-tooltip').show();
+            return;
         }
-    }
+
+        let roomId = $(this).attr('data-room-id');
+        let mid = $(this).attr('data-anchor-id');
+        let anchorInfo = await getAnchorInfo(mid);
+        $(this).addClass('isObserved');
+
+        let tooltip = $('<div>', {
+            class: "medal-anchor-tooltip",
+            style: "position: absolute; top: 0; left: 0; background-color: #fff; border: 1px solid #000; padding: 5px; z-index: 999; display: flex; align-items: center; text-decoration: none; color: black",
+            html: `
+                <a herf="https://live.bilibili.com/${roomId}" target="_blank">
+                    <img src="${anchorInfo.face}" style="border-radius: 50%; width: 64px; height: 64px">
+                    <div>${anchorInfo.name}</div>
+                </a>
+            `
+        });
+
+        $(this).append(tooltip);
+    });
+
+    $(document).on('mouseleave', '.fans-medal-item-ctnr.isObserved', function (e) {
+        $(this).find('.medal-anchor-tooltip').hide();
+    });
 
 })().catch(console.warn);
-
-
